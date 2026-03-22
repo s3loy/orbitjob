9537e5a9e0ec7d3af5aee2b9c119b62012a3d9f9
s3loy <1915276056@qq.com>
2026-03-23 05:28:13 +0800

    feat(metrics): add metrics for job creation and instance state transitions

--- internal/store/postgres/db.go
+++ internal/store/postgres/db.go
@@ -2,11 +2,21 @@ package postgres
 
 import (
 	"database/sql"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
 // Open creates a PostgreSQL DB handle from DSN.
 func Open(dsn string) (*sql.DB, error) {
-	return sql.Open("postgres", dsn)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	return db, nil
 }
